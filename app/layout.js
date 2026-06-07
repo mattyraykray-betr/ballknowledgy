9baab1ea5a838da21a9f44684c78227444204d62
mattyraykray-betr <matt@betranalytics.com>
2026-06-07 00:53:54 -0400

    Include Material Symbols font in RootLayout
    Add Material Symbols font link to the head section

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
+          rel="stylesheet"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );

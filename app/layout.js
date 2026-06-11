9cde0e2183e62ef35c7236aae7a3af0656966b84
mattyraykray-betr <matt@betranalytics.com>
2026-06-11 12:37:24 -0400

    Fix typo in Roboto Slab font link

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700;800;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700;800;900&amp;display=swap"
           rel="stylesheet"
         />            
       </head>

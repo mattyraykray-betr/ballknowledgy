0cb580a61e98e8aca796646cdf37783b7168572f
mattyraykray-betr <matt@betranalytics.com>
2026-06-16 10:56:29 -0400

    Refactor Twitter widget loading and update links

--- components/XListFeed.js
+++ components/XListFeed.js
@@ -6,27 +6,22 @@ export default function XListFeed({ darkMode }) {
   const containerRef = useRef(null);
 
   useEffect(() => {
-    function loadWidgets() {
+    function renderTimeline() {
       if (window.twttr?.widgets && containerRef.current) {
         window.twttr.widgets.load(containerRef.current);
       }
     }
 
-    const existingScript = document.querySelector(
-      'script[src="https://platform.twitter.com/widgets.js"]'
-    );
-
-    if (existingScript) {
-      loadWidgets();
-      return;
+    if (!document.querySelector('script[src="https://platform.twitter.com/widgets.js"]')) {
+      const script = document.createElement("script");
+      script.src = "https://platform.twitter.com/widgets.js";
+      script.async = true;
+      script.charset = "utf-8";
+      script.onload = renderTimeline;
+      document.body.appendChild(script);
+    } else {
+      renderTimeline();
     }
-
-    const script = document.createElement("script");
-    script.src = "https://platform.twitter.com/widgets.js";
-    script.async = true;
-    script.charset = "utf-8";
-    script.onload = loadWidgets;
-    document.body.appendChild(script);
   }, [darkMode]);
 
   return (
@@ -35,10 +30,9 @@ export default function XListFeed({ darkMode }) {
         className="twitter-timeline"
         data-height="520"
         data-theme={darkMode ? "dark" : "light"}
-        data-chrome="noheader nofooter noborders transparent"
-        href="https://twitter.com/i/lists/2066890467086598373"
+        href="https://x.com/i/lists/2066890467086598373"
       >
-        Latest posts
+        Posts from That Guy Rocked
       </a>
     </div>
   );

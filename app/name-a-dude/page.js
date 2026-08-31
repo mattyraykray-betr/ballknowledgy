279a245e6124695629579a62f9e02794fb3a4d93
mattyraykray-betr <matt@betranalytics.com>
2026-08-31 12:39:03 -0400

    Update SponsorBanner link and alt text

--- app/name-a-dude/page.js
+++ app/name-a-dude/page.js
@@ -1160,12 +1160,12 @@ export default function NameADudePage() {
         <SponsorBanner
           theme={theme}
           darkMode={darkMode}
-          href="https://x.com/NBAGuyRocked"
+          href="https://shop.thatguyrocked.com/"
           desktopLightSrc="/sponsor-desktop-light.png"
           desktopDarkSrc="/sponsor-desktop-dark.png"
           mobileLightSrc="/sponsor-mobile-light.png"
           mobileDarkSrc="/sponsor-mobile-dark.png"
-          alt="Sponsor name"
+          alt="Shop.ThatGuyRocked.com"
         />
       </div>
     </main>
